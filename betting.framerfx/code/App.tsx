7793dae97e1b9af809bbdcc1d6e232dc806722e8
Daniel Caine <audaci7y@gmail.com>
2019-03-07 15:23:22 -0800

    Data in own file

--- betting.framerfx/code/App.tsx
+++ betting.framerfx/code/App.tsx
@@ -1,76 +1,7 @@
 import * as React from "react";
-import { Data, Override } from 'framer';
 import { EventList } from './EventList'
 import { BetSlip } from './BetSlip'
 
-const data = Data({ 
-  
-  events: [
-    {
-      home: {
-        name: 'Real Madrid',
-        odds: 1.69
-      },
-      away: {
-        name: 'Ajax',
-        odds: 5.00
-      },
-      draw: {
-        name: 'Draw',
-        odds: 4.20
-      },
-    },
-    {
-      home: {
-        name: 'Derby County',
-        odds: 2.00
-      },
-      away: {
-        name: 'Wigan Athletic',
-        odds: 3.00
-      },
-      draw: {
-        name: 'Draw',
-        odds: 4.35
-      },
-    },
-    {
-      home: {
-        name: 'Fullham',
-        odds: 3.00
-      },
-      away: {
-        name: 'Chelsea FC',
-        odds: 5.00
-      },
-      draw: {
-        name: 'Draw',
-        odds: 4.35
-      },
-    },
-    {
-      home: {
-        name: 'Celtic',
-        odds: 3.00
-      },
-      away: {
-        name: 'Rangers',
-        odds: 4.00
-      },
-      draw: {
-        name: 'Draw',
-        odds: 4.35
-      },
-    },
-  ]
-});
-
-
-export const fillData: Override = () => {
-  return {
-    events: data.events
-  };
-};
 
 type Props = { 
   events: any
