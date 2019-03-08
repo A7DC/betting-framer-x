64a1f49a0a45b70d2684cbdb953d6471ac951fcd
Daniel Caine <audaci7y@gmail.com>
2019-03-07 20:21:20 -0800

    basic functionality replicated

--- betting.framerfx/code/BetSlip.tsx
+++ betting.framerfx/code/BetSlip.tsx
@@ -15,8 +15,52 @@ const Bet = ({ chosenBet, event }) => (
 )
 
 export class BetSlip extends React.Component<Props> {
+
+  input: HTMLInputElement;
+
+  state = {
+    stake: 0,
+    totalOdds: 0,
+    payout: 0,
+    open: false,
+  }
+
+  componentDidUpdate(prevProps) {
+    const newProps = this.props
+    if (prevProps !== newProps) {
+      this.calculateTotal(this.state.stake, this.props.chosenBets)
+      if (this.props.chosenBets.filter(e => e.name).length > 0) {
+        this.setState({open: true})
+      }
+    }
+  }
+
+  calculateTotal = (stake, chosenBets) => {
+
+    let oddsArray = []
+    chosenBets.map((val, i) => { oddsArray.push(val.odds) })
+
+    const reducer = (acc, currVal) => acc + (isNaN(currVal) ? 0 : currVal);
+
+    this.setState({
+      totalOdds: oddsArray.reduce(reducer, 0),
+      payout: this.state.totalOdds * this.state.stake,
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({ stake: event.target.value }, () => {
+      this.calculateTotal(this.state.stake, this.props.chosenBets)
+    })
+  }
+
+  showBetslip = ({ chosenBets}) => {
+    console.log(chosenBets, 'chosenBets asdfsdg')
+  }
+
   render() {
     const { chosenBets } = this.props
+    const { totalOdds, payout } = this.state
     return (
     <div style={style}>
         {chosenBets.map((val, i) => {
@@ -30,6 +74,19 @@ export class BetSlip extends React.Component<Props> {
             )
           }
         })}
+        {this.state.open ? 
+          <div>
+            <input
+              value={this.state.stake}
+              onChange={this.handleChange}
+              placeholder='Type something...'
+              type='number'
+              ref={(input) => { this.input = input }}
+            />
+            {`The total odds are ${totalOdds}`}
+            {`The total payout is ${payout}`} 
+          </div> : null
+        }
     </div>
     )
   }
--- betting.framerfx/code/EventList.tsx
+++ betting.framerfx/code/EventList.tsx
@@ -8,60 +8,9 @@ type Props = {
 }
 
 export class EventList extends React.Component<Props> {
-  input: HTMLInputElement;
-
-  state = {
-    stake: undefined,
-    totalOdds: 0,
-    payout: 0,
-  }
-
-  componentDidUpdate(prevProps) {
-    const newProps = this.props
-    if (prevProps !== newProps) { 
-      console.log(prevProps, 'prevProps')
-      this.calculateTotal(this.state.stake, this.props.chosenBets)
-      // this.calculateWinBonus()
-    }
-  }
-
-  // calculateWinBonus = () => {
-  //   const { chosenBets } = this.props
-  //   if (chosenBets.length >= 5 && chosenBets.length <= 9) {
-  //     console.log('5-9 legs')
-  //   }
-  //   if (chosenBets.length >= 10 && chosenBets.length <= 19) {
-  //     console.log('10-19 legs')
-  //   }
-  //   if (chosenBets.length >= 20 && chosenBets.length <= 30) {
-  //     console.log('20-30 legs')
-  //   }
-  // }
-  
-  calculateTotal = (stake, chosenBets) => {
-
-    let oddsArray = []    
-    chosenBets.map((val, i) => { oddsArray.push(val.odds) })
-
-    // console.log(chosenBets, 'chosenBets from EventList.jsx')
-    
-    const reducer = (acc, currVal) => acc + (isNaN(currVal) ? 0 : currVal);
-
-    this.setState({ 
-      totalOdds: oddsArray.reduce(reducer, 1),
-      payout: this.state.totalOdds * this.state.stake,
-    })
-  }
-
-  handleChange = (event) => {
-    this.setState({ stake: event.target.value }, () => {
-      this.calculateTotal(this.state.stake, this.props.chosenBets)
-    })
-  }
 
   render() {
     const { chosenBets, getSelection, events } = this.props
-    const { totalOdds, payout } = this.state
     return (
       <div style={style}>
         {events.map((val, i) => {
@@ -79,15 +28,6 @@ export class EventList extends React.Component<Props> {
             />
           )
         })}
-        <input
-          value={this.state.stake}
-          onChange={this.handleChange}
-          placeholder='Type something...'
-          type='number'
-          ref={(input) => { this.input = input }}
-        />
-        {`The total odds are ${totalOdds}`}
-        {`The total payout is ${payout}`}
       </div>
     )
   }
