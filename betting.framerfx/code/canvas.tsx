fd22d71090dda0661c0ff44da7b52f8152fcc3d7
Daniel Caine <audaci7y@gmail.com>
2019-03-12 12:02:07 -0700

    deleted the code files as , final design

--- betting.framerfx/code/canvas.tsx
+++
@@ -1,6 +0,0 @@
-// WARNING: this file is auto generated, any changes will be lost
-import { createDesignComponent, CanvasStore } from "framer"
-const canvas = CanvasStore.shared(); // CANVAS_DATA;
-export const Event = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_BgxDzBwX_", {}, 296,79);
-export const Logo = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_N4C1yT2ZS", {}, 128,29);
-export const Logo_1 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_m0fwLVSZ1", {}, 96,15);
